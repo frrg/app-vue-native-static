ab45260d30e39d60843c98b425668db9603c215a
ferifahrul13 <ferifahrul1@gmail.com>
2020-04-13 02:46:02 +0700

    mengubah tampilan about

--- About.vue
+++ About.vue
@@ -1,14 +1,38 @@
 <template>
   <view class="container">
     <view class="container-judul">
-      <text class="judul b">Kelompok 6</text>
-      <text class="judul">Kelas 2A3 International Class</text>
-      <text class="judul">2020</text>
-      <text class="keterangan">Heny Tri Suryani (P1337420618010)</text>
-      <text class="keterangan">Fazanisa Zulfa Izzati (P1337420618040)</text>
-      <text class="keterangan">Sinta Widhi Kurniawati (P1337420618063)</text>
-      <text class="keterangan">Sonia Laila Maghfiroh (P1337420618072)</text>
-      <text class="keterangan">Novia Anggi Astuti (P1337420618074)</text>
+      <image class="img" :style="{ width:200,height:200 }" :source="require('./assets/logo.png')" />
+      <text class="judul b">Kelompok 6:</text>
+    </view>
+    <view class="container-isi">
+      <view class="content">
+      <text class="keterangan">Heny Tri Suryani</text>
+       <text class="keterangan npm">(P1337420618010)</text>
+       </view>
+      <view class="content">
+      <text class="keterangan">Fazanisa Zulfa Izzati</text>
+       <text class="keterangan npm">(P1337420618040)</text>
+       </view>
+       
+      <view class="content">
+      <text class="keterangan">Sinta Widhi Kurniawati</text>
+       <text class="keterangan npm">(P1337420618063)</text>
+       </view>
+       
+      <view class="content">
+      <text class="keterangan">Sonia Laila Maghfiroh</text>
+       <text class="keterangan npm">(P1337420618072)</text>
+       </view>
+       
+      <view class="content">
+      <text class="keterangan">Novia Anggi Astuti </text>
+       <text class="keterangan npm">(P1337420618074)</text>
+       </view>
+      
+    <view class="container-judul">
+      <text class="center">Kelas 2A3 International Class</text>
+      <text class="center">2020</text>
+    </view>
     </view>
     <view class="home">
       <button
@@ -37,9 +61,28 @@ export default {
 </script>
 
 <style>
+
+.content {
+  flex-direction: row;
+  max-width: 400;
+  justify-content: center;
+}
+
 .keterangan {
+  width: 190;
+  color:#000;
+  font-size:16;
+  justify-content: center;
+  align-items: center;
+  padding-left: 10;
+  padding-right: 10;
+}
+.npm {
+  text-align:right;
+}
+.center {
+  text-align:center;
   font-size: 16;
-  color: black;
 }
 .home {
   margin:30;
@@ -49,11 +92,12 @@ export default {
   font-weight: bold;
 }
 .judul {
-  font-size: 20;
-  color: darkolivegreen;
+  font-size: 16;
+  padding : 30;
 }
 .container-judul {
   margin-top:10;
+  align-items: center;
 }
 .container-body {
   margin-top:10;
